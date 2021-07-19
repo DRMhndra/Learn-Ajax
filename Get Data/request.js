93d207e766505c18cdbc5dafe97d83a799c77604
DRMhndra <dimas_rizki_mahendra@teknokrat.ac.id>
2021-07-19 11:08:59 +0700

    Get Data from API

---
+++ Get Data/request.js
@@ -0,0 +1,27 @@
+const buttonRequest = document.querySelector('.request-api');
+const showData = document.querySelector('.place-data');
+buttonRequest.addEventListener('click', () => {
+  const request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        const datas = JSON.parse(request.responseText);
+        datas.forEach(data => {
+          const elementPost = `<article class="blog-posts">
+          <header class="blog-header">
+            <h3>${data.title}</h3>
+          </header>
+          <div class="blog-body">
+            <p>${data.body}</p>
+          </div>
+        </article>`;
+        showData.innerHTML += elementPost;
+        });
+      } else {
+        showData.textContent = `Sorry can't load data because : ${request.status}`;
+      }
+    }
+  });
+  request.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+  request.send();
+});
